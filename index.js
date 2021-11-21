b00c432329984eef454d9cdbddab54b1b5699182
Tim Quimbo <tquimbo1@gmail.com>
2021-11-20 23:11:32 -0500

    commit tree 580c3beed89763ca4686072b5742af11f4bda570

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(num => num * -1);
+  }
+  
+  function mapToNoChange(sourceArray) {
+    return sourceArray.map(num => num);
+  }
+  
+  function mapToDouble(sourceArray) {
+    return sourceArray.map(num => num * 2);
+  }
+  
+  function mapToSquare(sourceArray) {
+    return sourceArray.map(num => num ** 2);
+  }
+  
+  const reducer = (accumulator, item) => {
+    return accumulator + item;
+  }
+  
+  function reduceToTotal(sourceArray, startingPoint=0) {
+    return sourceArray.reduce(reducer, startingPoint);
+  }
+  
+  function turnArrayTrue(x, y) {
+    return !!x && !!y;
+  }
+  
+  function reduceToAllTrue(array)  {
+    return array.reduce(turnArrayTrue);
+  }
+  
+  function turnArrayFalse(x, y) {
+    return !!x || !!y;
+  }
+  
+  function reduceToAnyTrue(array){
+    return array.reduce(turnArrayFalse)
+  }
+
+  
